9ba2ee8934333e62490be6cdce12e89ece10a93b
Max Beuvelet <maxbeuvelet@gmail.com>
2025-12-01 19:41:54 +0000

    File changes

--- src/components/home/HeroArena.jsx
+++ src/components/home/HeroArena.jsx
@@ -30,8 +30,9 @@ export default function HeroArena() {
             minWidth: 200.00,
             scale: 1.00,
             scaleMobile: 1.00,
-            color: 0x3f3fff,
+            color: 0xff3b81,
             color2: 0xffffff,
+            size: 1,
             backgroundColor: 0x23153c
           });
         }
